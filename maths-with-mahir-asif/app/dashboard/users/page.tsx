c5165f943b75fc9db7e5512216ef8e308329d8a3
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-08-24 17:49:36 +0600

    nested routing

---
+++ maths-with-mahir-asif/app/dashboard/users/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h2>Dashboard Users</h2>
+
+      <ul className='mt-4'>
+        <li>User 1</li>
+        <li>User 2</li>
+        <li>User 3</li>
+        <li>User 4</li>
+      </ul>
+    </div>
+  )
+}
+
+export default page
