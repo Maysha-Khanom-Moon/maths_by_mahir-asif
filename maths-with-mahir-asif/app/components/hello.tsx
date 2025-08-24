bf376ebd252cf7cc59826190f49044faef61b3a8
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-08-24 17:36:36 +0600

    client components for client interaction

---
+++ maths-with-mahir-asif/app/components/hello.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+function Hello() {
+
+    console.log('I am a Client Component');
+
+    return (
+        <h1 className="text-2xl my-2">Hello Next.js</h1>
+    );
+}
+
+export default Hello;
